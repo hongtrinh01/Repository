9f162f1d9cd1de46e7c56bccbec4a742f0a1a0bf
hongtrinh01 <trinh.nth.57cntt@ntu.edu.vn>
2018-04-24 20:41:01 +0700

    he dieu hanh linux

---
+++ dayso.py
@@ -0,0 +1,8 @@
+#n=input("n=")
+for i in range(1,10+1):
+	print i
+'''S=0
+for i in range(1,n+1):
+	if(i%2==0):
+		S=S+i
+print S'''
---
+++ sinhvien.py
@@ -0,0 +1,29 @@
+class sinhvien:
+	def __init__(self,ten,namsinh,khoa):
+		self.ten=ten
+		self.namsinh=namsinh
+		self.khoa=khoa
+	def foot(self):
+		print("Gobble")
+	def getten(self):
+		return self.ten
+	def getnamsinh(self):
+		return self.namsinh
+	def getkhoa(self):
+		return self.khoa
+	def setten(self):
+		 self.ten=ten
+	def setnamsinh(self):
+		 self.namsinh=namsinh
+	def setkhoa(self):
+		 self.khoa=khoa
+	def disnhap(self):
+		 n=input("n= ")
+	def display(self):
+		print"ten: ",self.ten,"nam sinh: ",self.namsinh,"khoa:" ,self.khoa
+	dict={"ten":"trinh","namsinh":"1997","khoa":"57"}
+	print "sv: %s" %str(dict)
+
+
+	
+	
